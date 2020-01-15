65ada8717e07f92a78d711e6f76e1aede0b2a097
Thong Phan <thongphan@Ans-MacBook-Pro.local>
2020-01-15 22:13:00 +0700

    Updates

---
+++ precache-manifest.1546b43c2aaf7b3fd978637cd3ef2816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8d033315a4913c88ceacb7d26570b0",
+    "url": "/sentry-examplegit/index.html"
+  },
+  {
+    "revision": "34852263c33493a50a86",
+    "url": "/sentry-examplegit/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6d70d989f8f2f403171e",
+    "url": "/sentry-examplegit/static/js/2.c0470ac0.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/sentry-examplegit/static/js/2.c0470ac0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34852263c33493a50a86",
+    "url": "/sentry-examplegit/static/js/main.a50da519.chunk.js"
+  },
+  {
+    "revision": "5ddc05d1bd82c9146b16",
+    "url": "/sentry-examplegit/static/js/runtime-main.fd1eec94.js"
+  }
+]);
