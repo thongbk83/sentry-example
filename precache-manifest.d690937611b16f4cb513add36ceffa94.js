83ad11e7411894447ea765e9dfe1cace1ffacc82
Thong Phan <thongphan@Ans-MacBook-Pro.local>
2020-01-15 22:01:52 +0700

    Updates

---
+++ precache-manifest.d690937611b16f4cb513add36ceffa94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b15a5aa499b60f8dc42972371249b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1312bc6f9204d21f637c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6d70d989f8f2f403171e",
+    "url": "/static/js/2.c0470ac0.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/2.c0470ac0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1312bc6f9204d21f637c",
+    "url": "/static/js/main.f4a85489.chunk.js"
+  },
+  {
+    "revision": "0a856adb6a1f6b94e21d",
+    "url": "/static/js/runtime-main.bd0fd1eb.js"
+  }
+]);
